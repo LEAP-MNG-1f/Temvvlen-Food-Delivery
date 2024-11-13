4401df7bbd535c5c10c024326be14529ef8e99da
Crown7209 <146717132+Crown7209@users.noreply.github.com>
2024-11-13 12:54:53 +0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors());
+
+app.use("/", (request, response) => {
+  response.send(["Hello", "World"]);
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://temuulen-food-delivery.onrender.com/"
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <div></div>
