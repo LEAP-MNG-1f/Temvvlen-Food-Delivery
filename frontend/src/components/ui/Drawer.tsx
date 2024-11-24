02547bb1934076b28b3a5e0f452e08ace017faf8
Crown7209 <146717132+Crown7209@users.noreply.github.com>
2024-11-24 23:32:16 +0800

    ...

--- frontend/src/components/ui/Drawer.tsx
+++ frontend/src/components/ui/Drawer.tsx
@@ -33,6 +33,7 @@ export default function ShopDrawer() {
           </p>
           <div className="w-12 h-12"></div>
         </div>
+        <div className="w-full h-[1px] bg-[#D6D8DB]"></div>
       </div>
       <div className="w-[586px] h-[172px] shadow-[0px_-4px_8px_0px_rgba(187,190,205,0.20)] px-8 py-[10px] flex items-center gap-[10px] fixed bottom-0 z-10">
         <div className="w-full flex flex-col">
---
+++ frontend/src/components/ui/DrawerCard.tsx
@@ -0,0 +1,7 @@
+export const DrawerCard = () => {
+  return (
+    <div className="py-6 w-full border-b border-[#D6D8DB]">
+      <div className="p-4"></div>
+    </div>
+  );
+};
