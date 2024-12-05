ed16b504b974d0142500b15ef74516d76688ab00
Crown7209 <146717132+Crown7209@users.noreply.github.com>
2024-12-05 10:51:07 +0800

    ...

--- backend/server.js
+++ backend/server.js
@@ -20,35 +20,35 @@ app.use(bodyParser.json());
 
 //--------------------Cloudinary--------------------//
 
-cloudinary.config({
-  cloud_name: "dsjr8e4oe",
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: "dsjr8e4oe",
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
-(async function () {
-  // Upload an image
-  const result = await cloudinary.uploader.upload(
-    "https://res.cloudinary.com/dsjr8e4oe/image/upload/v1731982251/417dc4f856c8e6327152022aab0504fb_ykjibf.png"
-  );
-  // console.log(result);
+// (async function () {
+//   // Upload an image
+//   const result = await cloudinary.uploader.upload(
+//     "https://res.cloudinary.com/dsjr8e4oe/image/upload/v1731982251/417dc4f856c8e6327152022aab0504fb_ykjibf.png"
+//   );
+//   // console.log(result);
 
-  // Optimize delivery by resizing and applying auto-format and auto-quality
-  const optimizeUrl = cloudinary.url(result.public_id, {
-    fetch_format: "auto",
-    quality: "auto",
-  });
-  // console.log(optimizeUrl);
+//   // Optimize delivery by resizing and applying auto-format and auto-quality
+//   const optimizeUrl = cloudinary.url(result.public_id, {
+//     fetch_format: "auto",
+//     quality: "auto",
+//   });
+//   // console.log(optimizeUrl);
 
-  // Transform the image: auto-crop to square aspect_ratio
-  const autoCropUrl = cloudinary.url(result.public_id, {
-    crop: "auto",
-    gravity: "auto",
-    width: 500,
-    height: 500,
-  });
-  // console.log(autoCropUrl);
-})();
+//   // Transform the image: auto-crop to square aspect_ratio
+//   const autoCropUrl = cloudinary.url(result.public_id, {
+//     crop: "auto",
+//     gravity: "auto",
+//     width: 500,
+//     height: 500,
+//   });
+//   // console.log(autoCropUrl);
+// })();
 
 //--------------------Router--------------------//
 
